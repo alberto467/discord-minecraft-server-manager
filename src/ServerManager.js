6e97f39d77edcc2927eec5c32beccbb5da3ecfc0
alberto467 <alberto467@hotmail.it>
2019-06-09 22:13:13 +0200

    Switched to streamparser

--- src/ServerManager.js
+++ src/ServerManager.js
@@ -1,6 +1,8 @@
 import { spawn } from 'child_process'
 import EventEmitter from 'events'
 
+import StreamParser from './StreamParser'
+
 export const ServerStatus = {
   NOT_RUNNING: 'NOT_RUNNING',
   STARTING: 'STARTING',
@@ -20,27 +22,29 @@ export default class ServerManager extends EventEmitter {
 
       this.server = spawn('cmd.exe', [ '/c', process.env.MINECRAFT_SERVER_STARTUP_SCRIPT_PATH ])
 
-      this.server.stdout.on('data', data => {
-        data = data.toString('utf8')
-
-        if (data.match(/\[Server thread\/INFO\]: Done \(.+?\)!/)) {
-          this.status = ServerStatus.RUNNING
-          console.log('Server Launched')
-          return fulfil()
-        }
-
-        const joined = data.match(/\[Server thread\/INFO\]: (.+?) joined the game/)
-        if (joined) {
-          this.emit('playerJoined', joined[1])
-          return console.log(`Player joined: ${joined[1]}`)
-        }
-
-        const left = data.match(/\[Server thread\/INFO\]: (.+?) left the game/)
-        if (left) {
-          this.emit('playerLeft', left[1])
-          return console.log(`Player left: ${left[1]}`)
-        }
+      this.streamParser = new StreamParser({
+        launched: /\[Server thread\/INFO\]: Done \(.+?\)!/,
+        joined: /\[Server thread\/INFO\]: (.+?) joined the game/,
+        left: /\[Server thread\/INFO\]: (.+?) left the game/
       })
+
+      this.streamParser.on('launched', () => {
+        this.status = ServerStatus.RUNNING
+        console.log('Server Launched')
+        return fulfil()
+      })
+
+      this.streamParser.on('joined', match => {
+        this.emit('playerJoined', match[1])
+        return console.log(`Player joined: ${match[1]}`)
+      })
+
+      this.streamParser.on('left', match => {
+        this.emit('playerLeft', match[1])
+        return console.log(`Player left: ${match[1]}`)
+      })
+
+      this.server.stdout.pipe(this.streamParser)
 
       this.server.on('exit', code => {
         if (code !== 0)
---
+++ src/StreamParser.js
@@ -0,0 +1,26 @@
+import { Writable } from 'stream'
+
+export default class StreamParser extends Writable {
+  constructor(regexs) {
+    super()
+
+    this.setRegexs(regexs)
+  }
+
+  _write(data, enc, next) {
+    if (Buffer.isBuffer(data))
+      data = data.toString('utf8')
+
+    console.log(data)
+
+    for (const [ key, regex ] of this.regexs) {
+      const match = data.match(regex)
+      if (match) return this.emit(key, match)
+    }
+    next()
+  }
+
+  setRegexs(regexs) {
+    this.regexs = Object.entries(regexs)
+  }
+}
